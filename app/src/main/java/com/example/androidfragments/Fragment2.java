8879d652e9dccfb8488788943d99e49b83afc2b5
Bhagya <chalanibhagya5@gmail.com>
2020-03-26 20:14:40 +0530

    AndroidFragments

---
+++ app/src/main/java/com/example/androidfragments/Fragment2.java
@@ -0,0 +1,35 @@
+package com.example.androidfragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ *
+ */
+public class Fragment2 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+
+    }
+
+
+
+
+
+
+
+}
